7836cf1182f5d305457818cb88f5924fa293f839
Eston <estonpillay@gmail.com>
2019-10-06 14:47:38 +0200

    Revert "Merge pull request #3 from eston-p/change-last-name-from-being-returned"
    This reverts commit 492e73f06bd04522868c4abd78d5a5da91890b10, reversing
    changes made to dfac6e628c06e59a8db8e0bd8fd04de38987a8b8.

--- index.php
+++ index.php
@@ -7,5 +7,9 @@ $class = new MyRevert('Alan Walker');
 
 $class->seperateName();
 
+<<<<<<< HEAD
 
 echo 'Hello, FirstName: ' . $class->getFirstName();
+=======
+echo 'Hello, FirstName: ' . $class->getFirstName() . ' and Lastname: ' . $class->getLastName();
+>>>>>>> parent of 492e73f... Merge pull request #3 from eston-p/change-last-name-from-being-returned
