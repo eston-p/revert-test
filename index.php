af2ad1374131a5555d2fc8a18088bd42d32d3e07
Eston <estonpillay@gmail.com>
2019-10-06 14:36:27 +0200

    Revert "Merge pull request #2 from eston-p/simplify-separation-of-name"
    This reverts commit dfac6e628c06e59a8db8e0bd8fd04de38987a8b8, reversing
    changes made to fe6156d7917538cd185d304b7ac59827b4d75e8c.

--- index.php
+++ index.php
@@ -7,8 +7,5 @@ $class = new MyRevert('Alan Walker');
 
 $class->seperateName();
 
-<<<<<<< HEAD
+
 echo 'Hello, FirstName: ' . $class->getFirstName();
-=======
-echo 'Hello, FirstName: ' . $class->returnMyFirstandLastName()[0] . ' and Lastname: ' . $class->returnMyFirstandLastName()[1];
->>>>>>> parent of dfac6e6... Merge pull request #2 from eston-p/simplify-separation-of-name
